81e3c75bf1610e80bd00dfbfe1ee9febe4b0b663
to4to <justjustifying@outlook.com>
2024-05-13 22:21:39 +0530

    refactor: remove unused code and add missing return type in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+
+use std::fs;
+use std::io;
+
+
 fn main() {
-    println!("Hello, world!");
+   // println!("Hello, world!");
+
+
+std::process::exit(real_main());
+}
+
+
+fn real_main()->i32{
+    
 }
