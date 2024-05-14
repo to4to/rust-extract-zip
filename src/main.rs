fa84e75b8a55b2d7281cca50a2622aa51bdd01bc
to4to <justjustifying@outlook.com>
2024-05-14 23:02:33 +0530

    refactor: Add command line argument parsing and error handling in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
-
 use std::fs;
 use std::io;
 
-
 fn main() {
-   // println!("Hello, world!");
+    // println!("Hello, world!");
 
-
-std::process::exit(real_main());
+    std::process::exit(real_main());
 }
 
+fn real_main() -> i32 {
+    let args: Vec<_> = std::env::args().collect();
 
-fn real_main()->i32{
-    let args:Vec<_>=std::env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: {} <file>", args[0]);
+        return 1;
+    }
+
+    
+
+
+    let fname=std::path::Path::new(&*args[1]);
+
 }
