3eebb54562857fef406e912963701be374f7e481
to4to <justjustifying@outlook.com>
2024-05-16 06:27:05 +0530

    refactor: Improve file handling and add zip archive extraction in main.rs

--- src/main.rs
+++ src/main.rs
@@ -16,27 +16,29 @@ fn real_main() -> i32 {
         return 1;
     }
 
-    
-
-
-    let fname=std::path::Path::new(&*args[1]);
-    let file =fs::File::open(&fname).unwrap();
-
+    let fname = std::path::Path::new(&*args[1]);
+    let file = fs::File::open(&fname).unwrap();
 
     let mut archive = zip::ZipArchive::new(file).unwrap();
 
+    for i in 0..archive.len() {
+        let mut file = archive.by_index(i).unwrap();
 
-    for i in 0..archive.len(){
-
-        let mut file= archive.by_index(i).unwrap(); 
-
-
-        let outpath=match file.enclosed_name(){
-
-
-            
+        let outpath = match file.enclosed_name() {
+            Some(path) => path.to_owned(),
+            None => continue,
+        };
+        {
+            let comment = file.comment();
+            if !comment.is_empty() {
+                println!("File {} comment: {}", i, comment);
+            }
         }
 
-    }
 
+        
+        
+    }//loop
+
+    return 0;
 }
